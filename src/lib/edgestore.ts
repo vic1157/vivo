026af68c041e5cdf1a3268c98186476c5374523f
David Odejimi <david.odejimi677@gmail.com>
2024-09-30 17:57:49 -0500

    chore: Remove unused LabAnalyzer and EdgeStore files

--- src/lib/edgestore.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { type EdgeStoreRouter } from "@/app/api/edgestore/[...edgestore]/route";
-import { createEdgeStoreProvider } from "@edgestore/react";
-
-export const { EdgeStoreProvider, useEdgeStore } =
-  createEdgeStoreProvider<EdgeStoreRouter>({
-    maxConcurrentUploads: 2,
-  });
