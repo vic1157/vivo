2ce849ddec9c7d9f98eb17a7bbe1ca8f227c6f14
David Odejimi <david.odejimi677@gmail.com>
2024-09-30 16:33:25 -0500

    chore: Update npm dependencies and optimize build process

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
-// next.config.js
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
-    images: {
-      domains: ['via.placeholder.com'],
-    },
-  };
-  
+  // Images configuration
+  images: {
+    domains: ['via.placeholder.com'],
+  },
+
+  // Disable SWC minification
+  swcMinify: false,
+
+  // Webpack configuration for minification
+  webpack(config) {
+    config.optimization.minimizer = [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            ascii_only: true,
+          },
+        },
+      }),
+    ];
+    return config;
+  },
+};
